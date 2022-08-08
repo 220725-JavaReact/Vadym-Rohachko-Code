8a81e871034df20d06b65d54a261fa19168a14ff
vadym <vadym582@revature.net>
2022-08-08 08:52:21 +0300

    Sec

--- p0/Main/Util/SecretClass.java
+++ p0/Main/Util/SecretClass.java
@@ -1,7 +1,7 @@
 package Util;
 
 class SecretClass {
-	static final String url = "jdbc:postgresql://database-3.cwukqvldiavm.us-east-1.rds.amazonaws.com:5432/OnlineStore";
-	static final String userName = "vadym";
-	static final String password = "demopassword";
+	static final String url = "";
+	static final String userName = "";
+	static final String password = "";
 }
