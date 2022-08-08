b780c2ba462cfd31772deb27c3f34217f53dab46
vadym <vadym582@revature.net>
2022-08-09 00:36:27 +0300

    Sec

--- p0/OnlineStore/Util/SecretClass.java
+++ p0/OnlineStore/Util/SecretClass.java
@@ -2,6 +2,6 @@ package Util;
 
 class SecretClass {
 	static final String url = "";
-	static final String userName = "";
+	static final String userName = " ";
 	static final String password = "";
 }
